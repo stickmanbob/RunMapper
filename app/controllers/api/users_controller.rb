cc33cf5fccdc0103cd7d594ebf5b1ce9a417f604
Ajay Rajamani <ajayrajamani@gmail.com>
2020-06-28 01:37:08 -0700

    Login user on sign up

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -4,6 +4,7 @@ class Api::UsersController < ApplicationController
         @user = User.new(user_params)
 
         if @user.save
+            login_user!(@user)
             render :show
         else
             render json: @user.errors, status: 401
