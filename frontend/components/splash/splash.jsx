8e960249436eec810658ea32a141320a242867f5
Ajay Rajamani <ajayrajamani@gmail.com>
2020-07-01 12:34:17 -0700

    Add background 2 to splash

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -35,7 +35,47 @@ export default function Splash () {
 
             <section className="splash-background two">
                 <div className="splash-header">
-                    
+                    <div className="splash-header" >
+                        <hr />
+                        <h1>Find Your Way Anywhere</h1>
+                        <hr />
+
+                        <p>
+                            Map workouts anywhere on the globe. 
+                            Save your favorites for the next time
+                            you're ready to go.
+                        </p>
+
+                        <div className="location-lists">
+                            <ul>
+                                <li>New York, NY</li>
+                                <li>Manhattan, NY</li>
+                                <li>San Fransicso, CA</li>
+                                <li>Austin, TX</li>
+                                <li>Denver, CO</li>
+                                <li>Seattle, WA</li>
+                            </ul>
+
+                            <ul>
+                                <li>San Diego, CA</li>
+                                <li>Portland, OR</li>
+                                <li>Chicago, IL</li>
+                                <li>Honolulu, HI</li>
+                                <li>Tallahassee, Fl</li>
+                                <li>Boston, MA</li>
+                            </ul>
+
+                            <ul>
+                                <li>Dublin, Ireland</li>
+                                <li>Christchurch, New Zealand</li>
+                                <li>Vancouver, Canada</li>
+                                <li>Edinburgh, Scotland</li>
+                                <li>United States</li>
+                                <li>Find Your City</li>
+                            </ul>
+                        </div>
+
+                    </div>
                 </div>
             </section>
         </div>
